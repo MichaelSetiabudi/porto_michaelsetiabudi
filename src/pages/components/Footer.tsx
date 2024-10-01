ae619ec6dee84a9b22dd7333c499b7b8b1b3e6ee
Michael Setiabudi <michael.s22@mhs.istts.ac.id>
2024-10-01 10:12:16 +0700

    update front_end

--- src/pages/components/Footer.tsx
+++ src/pages/components/Footer.tsx
@@ -1,14 +1,9 @@
 // Header.tsx
 
-import React, { useState, useEffect } from "react";
+import React from "react";
 import {
-  Navbar,
-  Nav,
   Container,
-  Button,
 } from "react-bootstrap";
-import Image from "next/image";
-import { useRouter } from "next/router";
 import style from "@/styles/Footer.module.css"
 
 const Footer: React.FC = () => {
