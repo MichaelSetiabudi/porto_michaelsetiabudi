901cdbc5ee4aac6d110fab84feb6442c5cba63c0
Michael Setiabudi <michael.s22@mhs.istts.ac.id>
2025-02-22 21:34:32 +0700

    changed tailwind from cdn to download

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script src="https://cdn.tailwindcss.com"></script>
+        
       </Head>
+
       <body>
         <Main />
         <NextScript />
